deab5c3219e57572c08f61d8ed92558d1933dc38
John Ellis <john@deckerego.net>
2020-09-04 07:07:43 -0400

    Fix issue where using a modern browser will lock up Bottle's HTTP server

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -7,5 +7,5 @@ from bottle import run
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 7413))
-	run(application, reloader = False, host = '0.0.0.0', port = port, quiet = False)
+	run(application, server='paste', reloader = False, host = '0.0.0.0', port = port, quiet = False)
 	application.close()

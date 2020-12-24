41bbe67c5d64a7577439c2729f270d63cbed1d62
John Ellis <john@deckerego.net>
2020-12-24 17:03:54 -0500

    Long overdue version bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ base_data_files = [
 
 setup(
     name='tallypi',
-    version='0.3.2',
+    version='0.4.1',
     description='A network controlled tally light for video cameras',
     author='DeckerEgo',
     author_email='john@deckerego.net',

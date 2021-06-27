f8e7c790ccb12d3e935b4c8611bb3b9e5070e45e
John Ellis <john@deckerego.net>
2021-06-27 10:01:10 -0400

    Version bump (again)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ base_data_files = [
 
 setup(
     name='tallypi',
-    version='0.4.4',
+    version='0.4.5',
     description='A network controlled tally light for video cameras',
     author='DeckerEgo',
     author_email='john@deckerego.net',

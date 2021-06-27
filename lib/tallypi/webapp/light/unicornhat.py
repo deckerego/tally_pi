7d6be87b42caeaa948f3960b400d87bb50af9ed6
John Ellis <john@deckerego.net>
2021-06-27 17:22:35 -0400

    Re-arrange GPIO and display modules so they can be mocked for easier testing (#28)

--- lib/tallypi/webapp/light/unicornhat.py
+++ lib/tallypi/webapp/light/unicornhat.py
@@ -1,11 +1,11 @@
 import unicornhat as uh
-import time
+from tallypi.webapp.light.base import AbstractLight
 import logging
 import inspect
 
 logger = logging.getLogger('light')
 
-class Light():
+class Light(AbstractLight):
     name = 'light'
     keyword = 'light'
 
@@ -14,24 +14,6 @@ class Light():
 
     def __del__(self):
         uh.off()
-
-    def validateColor(self, intensity):
-        if intensity < 0:
-            return 0
-        elif intensity > 255:
-            return 255
-        else:
-            return intensity
-
-    def validateBrightness(self, intensity):
-        if intensity < 0.0:
-            return 0.0
-        if intensity > 0.0 and intensity < 0.2:
-            return 0.2
-        elif intensity > 1.0:
-            return 1.0
-        else:
-            return intensity
 
     def setColor(self, red, green, blue):
         uh.set_all(red, green, blue)
@@ -46,36 +28,6 @@ class Light():
 
     def getBrightness(self):
         return uh.get_brightness()
-
-    def goToColor(self, r, g, b, steps=4, wait=0.05):
-        if not steps:
-            steps = 1
-
-        red = self.validateColor(r)
-        green = self.validateColor(g)
-        blue = self.validateColor(b)
-
-        rSrc, gSrc, bSrc = self.getColor()
-        rStep = (red - rSrc) / steps
-        gStep = (green - gSrc) / steps
-        bStep = (blue - bSrc) / steps
-
-        while steps > 1:
-            rSrc, gSrc, bSrc = self.getColor()
-            self.setColor(rSrc + rStep, gSrc + gStep, bSrc + bStep)
-            steps -= 1
-            time.sleep(wait)
-
-        self.setColor(r, g, b)
-
-    def test(self):
-        self.setBrightness(0.5)
-        self.goToColor(255, 0, 0, 10, 0.1)
-        self.goToColor(0, 255, 0, 10, 0.1)
-        self.goToColor(0, 0, 255, 10, 0.1)
-        self.goToColor(255, 0, 0, 10, 0.1)
-        self.goToColor(255, 255, 255, 1, 0.01)
-        self.shutdown()
 
     def shutdown(self):
         uh.off()

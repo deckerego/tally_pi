fd6f7d1c38d97361763e26dca30698e8154b0331
John Ellis <john@deckerego.net>
2021-09-26 10:28:42 -0400

    Make sure HTTP connections are closed after invoke (#46)

--- lib/tallypi/webapp/routes.py
+++ lib/tallypi/webapp/routes.py
@@ -34,6 +34,10 @@ def _to_json(r, g, b, bright):
     return '{ "hostname": "%s", "red": %i, "green": %i, "blue": %i, "brightness": %f }' % (hostname, r, g, b, bright)
 
 @application.hook('after_request')
+def connection():
+    response.headers['Connection'] = 'close'
+
+@application.hook('after_request')
 def access_control():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
